403e72a9156dc9cc4a9e972beec138938e636ea5
GLstarsky <147454401+GLstarsky@users.noreply.github.com>
2025-04-28 07:22:03 +0700

    push
    add

---
+++ mxh/vite-project/src/Components/User/AddFriend.jsx
@@ -0,0 +1,155 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './AddFriend.css';
+
+function AddFriend() {
+    return (
+        <div>
+            <nav>
+                <div className="nav-left">
+                    <img src="./images/logo.png" alt="Logo" />
+                    <input type="text" placeholder="Tìm Kiếm..." />
+                </div>
+
+                <div className="nav-middle">
+                    <a href="#">
+                        <i className="fa-solid fa-home"></i>
+                    </a>
+
+                    <a href="#" className="active">
+                        <i className="fa-solid fa-user-friends"></i>
+                    </a>
+
+                    <a href="#">
+                        <i className="fa-solid fa-play-circle"></i>
+                    </a>
+
+                    <a href="#">
+                        <i className="fa-solid fa-users"></i>
+                    </a>
+                </div>
+
+                <div className="nav-right">
+                    <span className="profile"></span>
+
+                    <a href="#">
+                        <i className="fas fa-bell"></i>
+                    </a>
+
+                    <a href="#">
+                        <i className="fas fa-ellipsis-h"></i>
+                    </a>
+                </div>
+            </nav>
+
+            <div class="container-fluid">
+                <div class="left-panel">
+                    <ul>
+                        <li>
+                            <span>Bạn bè</span>
+                        </li>
+                        <li>
+                            <div class="icon-circle1"><i class="fa fa-user"></i></div>
+                            <span>Trang chủ</span>
+                        </li>
+                        <li>
+                            <div class="icon-circle"><i class="fa fa-user-plus"></i></div>
+                            <span>Lời mời kết bạn</span>
+                        </li>
+                        <li>
+                            <div class="icon-circle"><i class="fa fa-user-friends"></i></div>
+                            <span>Gợi ý</span>
+                        </li>
+                        <li>
+                            <div class="icon-circle"><i class="fa fa-flag"></i></div>
+                            <span>Tất cả bạn bè</span>
+                        </li>
+                        <li>
+                            <div class="icon-circle"><i class="fa fa-gift"></i></div>
+                            <span>Sinh nhật</span>
+                        </li>
+                        <li>
+                            <div class="icon-circle"><i class="fa fa-list"></i></div>
+                            <span>Danh sách tùy chỉnh</span>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="right-panel">
+                    {/* Section Lời mời kết bạn */}
+                    <div className="section">
+                        <div className="section-header">
+                            <h3>Lời mời kết bạn</h3>
+                            <button className="see-all">Xem tất cả</button>
+                        </div>
+                        
+                        <div className="user-cards">
+                            {/* User Card 1 */}
+                            <div className="user-card">
+                                <div className="user-image"></div>
+                                <div className="user-info">
+                                    <h4>User A</h4>
+                                    <p>3 bạn chung</p>
+                                    <div className="user-actions">
+                                        <button className="delete-btn">Xóa</button>
+                                        <button className="accept-btn">Chấp nhận</button>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            {/* User Card 2 */}
+                            <div className="user-card">
+                                <div className="user-image"></div>
+                                <div className="user-info">
+                                    <h4>User B</h4>
+                                    <p>5 bạn chung</p>
+                                    <div className="user-actions">
+                                        <button className="delete-btn">Xóa</button>
+                                        <button className="accept-btn">Chấp nhận</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <button className="see-more">Xem thêm</button>
+                    </div>
+
+                    {/* Section Những người bạn có thể biết */}
+                    <div className="section">
+                        <div className="section-header">
+                            <h3>Những người bạn có thể biết</h3>
+                            <button className="see-all">Xem tất cả</button>
+                        </div>
+                        
+                        <div className="user-cards">
+                            {/* User Card 1 */}
+                            <div className="user-card">
+                                <div className="user-image"></div>
+                                <div className="user-info">
+                                    <h4>User C</h4>
+                                    <p>2 bạn chung</p>
+                                    <button className="add-btn">Thêm bạn bè</button>
+                                </div>
+                            </div>
+                            
+                            {/* User Card 2 */}
+                            <div className="user-card">
+                                <div className="user-image"></div>
+                                <div className="user-info">
+                                    <h4>User D</h4>
+                                    <p>4 bạn chung</p>
+                                    <button className="add-btn">Thêm bạn bè</button>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <button className="see-more">Xem thêm</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AddFriend;
